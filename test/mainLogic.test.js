3e2c1b84b5c6d9877e42fb4c3514a6c0bff39928
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-06-16 21:01:16 +0100

    mocking the crossref api

--- test/mainLogic.test.js
+++ test/mainLogic.test.js
@@ -12,14 +12,18 @@ function numericTitle(){
   return '99 red balloons'
 }
 
-function longTitle() {
+function longTitle(){
     return 'This is a very long title that I am writing to test long titles'
 }
 
-function matchingShortTitle() {
+function matchingShortTitle(){
     return 'This is a very long title'
 }
 
+function loadCrossrefJSON(){
+  var json = require('./fixtures/crossref.json');
+  return json;
+}
 
 test.each([
   [simpleTitle(), 'a simple title'],
@@ -35,21 +39,18 @@ test.each([
 test('checks long first title matches short second title', () => {
 
   var result = ScholarDOI.titleLengthCompare(longTitle(), matchingShortTitle());
-
   expect(result).toBe(true);
 });
 
 test('checks short first title matches long second title', () => {
 
   var result = ScholarDOI.titleLengthCompare(matchingShortTitle(), longTitle());
-
   expect(result).toBe(true);
 });
 
 test('checks different titles do not match', () => {
 
   var result = ScholarDOI.titleLengthCompare(longTitle(), punctuatedTitle());
-
   expect(result).toBe(false);
 });
 
@@ -62,6 +63,19 @@ test('make a hyperlink object', () => {
 test('add a doi to a hyperlink', () => {
 
   var a = ScholarDOI.buildDOILink('10.1002/esp.3884');
-
   expect(a[0].href).toBe('https://doi2bib.org/bib/10.1002/esp.3884');
+});
+
+test('get doi from crossref query result', () => {
+
+  var doi = ScholarDOI.getDOI(loadCrossrefJSON());
+  expect(doi).toBe('10.1002/esp.3884');
+
+});
+
+test('get cleaned title from crossref query result', () => {
+
+  var doi = ScholarDOI.getTitleCrossref(loadCrossrefJSON());
+  expect(doi).toBe('how long is a hillslope ');
+
 });
